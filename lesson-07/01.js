ed38595cad2133a850e5d01ee4295efffa2ad777
Karina Chemodanova <k.mordinskaya@gmail.com>
2025-06-25 19:29:01 +0400

    first

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -13,7 +13,39 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+	'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+	'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+	'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+
+const slideCount = WEB_TECH_IMAGES.length
+let currentIndex = 0
+let currentImg = document.getElementById('web-tech-image')
+
+function updateSlide() {
+	let newSrc = WEB_TECH_IMAGES[currentIndex]
+	currentImg.setAttribute('src', newSrc)
+}
+
+nextButton.addEventListener('click', () => {
+	if (currentIndex < slideCount - 1) {
+		currentIndex++
+		updateSlide()
+	} else {
+		currentIndex = 0
+		updateSlide()
+	}
+})
+
+prevButton.addEventListener('click', () => {
+	if (currentIndex > 0) {
+		currentIndex--
+		updateSlide()
+	} else {
+		currentIndex = slideCount - 1
+		updateSlide()
+	}
+})
